4138d50d1f3f1675b41da926ef05d18f85c69d14
Бат-Эрдэнэ Б <n.baterdene.goodgodking@gmail.com>
2020-08-26 23:18:41 +0800

    add bullet

--- js/entities/bullet.js
+++ js/entities/bullet.js
@@ -1,1 +1,27 @@
-//TODO
+import { Renderer } from './renderer.js';
+export class Bullet {
+  constructor(y, x, direction) {
+    this.size = 8;
+    this.y = y;
+    this.x = x;
+    this.direction = direction;
+    this.body = Renderer(
+      this.size,
+      this.size,
+      (y + 0.5) * this.size,
+      (x + 0.5) * this.size,
+      'bullet'
+    );
+  }
+  move = () => {
+    let dirX = [0, 0.25, 0, -0.25],
+      dirY = [0.25, 0, -0.25, 0];
+    this.y += dirY[this.direction];
+    this.x += dirX[this.direction];
+    this.body.x = this.x * this.size;
+    this.body.y = this.y * this.size;
+  };
+}
+
+// a = Bullet(this.y, this.x, direction);
+// a.move;
